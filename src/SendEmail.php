69708bdc0220e08a063f485357a6e91c6434095f
Gaia28 <matheusgaia33@gmail.com>
2026-03-09 11:22:40 -0300

    feat: create SendEmail Class and functions: GetApi and sendEmail

--- src/SendEmail.php
+++ src/SendEmail.php
@@ -1,2 +1,75 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
 
+class sendEmail
+{
+
+public function getAPI(){
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+CURLOPT_URL => "https://liturgia.up.railway.app/v2/",
+CURLOPT_RETURNTRANSFER => true,
+CURLOPT_ENCODING => "",
+CURLOPT_MAXREDIRS => 10,
+CURLOPT_TIMEOUT => 30,
+CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+CURLOPT_CUSTOMREQUEST => "GET",
+CURLOPT_HTTPHEADER => [
+"Accept: */*",
+"User-Agent: Thunder Client (https://www.thunderclient.com)",
+],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+echo "cURL Error #:" . $err;
+} else {
+echo $response;
+}
+}
+
+public function SendEmail(){
+$mail = new PHPMailer\PHPMailer\PHPMailer(true);
+try{
+
+$mailer = $_ENV['MAIL_MAILER'];
+$scheme = $_ENV['MAIL_SCHEME'];
+$host = $_ENV['MAIL_HOST'];
+$port = $_ENV['MAIL_PORT'];
+$username = $_ENV['MAIL_USERNAME'];
+$password = $_ENV['MAIL_PASSWORD'];
+$fromAddress = $_ENV['MAIL_FROM_ADDRESS'];
+$fromName = $_ENV['MAIL_FROM_NAME'];
+
+$mail->isSMTP();
+$mail->Host = $host;
+$mail->SMTPAuth = true;
+$mail->Username = $username;
+$mail->Password = $password;
+$mail->SMTPSecure = $scheme;
+$mail->Port = $port;    
+$mail->setFrom($fromAddress, $fromName);
+$mail->addAddress('matheusgaia33@gmail.com','Matheus Gaia');
+
+$mail->isHTML(true);
+$mail->Subject = 'Testando o envio de email';
+$mail->Body = 'Olá, este é um email de teste enviado usando PHPMailer!';
+
+$mail->send();
+echo 'Email enviado com sucesso!';
+}catch (Exception $e){
+echo "Erro ao enviar email: {$mail->ErrorInfo}";
+}
+}
+}
+
+$sendEmail = new sendEmail();
+$sendEmail->getAPI();
+$sendEmail->SendEmail();
